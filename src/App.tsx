4302365c2f7c5c461ea267083bafb420f4310322
Ilshat <ilshat.kasimov1@gmail.com>
2025-09-22 16:43:30 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+
+  return (<>
+    <div>
+      first project nahui
+      <Button variant="destructive">sosew</Button>
+    </div>
+    
+  </>)
+}
+
+export default App
