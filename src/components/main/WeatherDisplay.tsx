ef0336da48abbe118de32ae001ed1f7a7e974814
Your Name <you@example.com>
2025-10-26 22:00:24 +0300

    edit Main.tsx

--- src/components/main/WeatherDisplay.tsx
+++ src/components/main/WeatherDisplay.tsx
@@ -13,30 +13,37 @@ export const WeatherDisplay = () => {
       {isLoading && <div>Загрузка погоды...</div>}
 
       {weather && (
-        <div className="flex justify-around px-8 ">
-
+        <div className="bg-blue-50 p-4 rounded">
+          <div className="flex justify-between">
+              <div className="flex flex-col">
+                <h1 className="capitalize text-5xl">{weather.weather[0].description}</h1>
+                <div>
+                  <span className="text-6xl">
+                    {Math.round(weather.main.temp)}°C
+                  </span>
+                </div>
+              </div>
+            </div>
+          <h4 className="font-bold text-xl">
+            {weather.name}, {weather.sys.country}
+          </h4>
+          <div className="flex items-center mt-2">
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+              alt={weather.weather[0].description}
+            />
+            <span className="text-2xl ml-2">
+              {Math.round(weather.main.temp)}°C
+            </span>
+          </div>
+          <p className="capitalize">{weather.weather[0].description}</p>
+          <div className="grid grid-cols-2 gap-2 mt-2 text-sm">
+            <div>Ощущается: {Math.round(weather.main.feels_like)}°C</div>
+            <div>Влажность: {weather.main.humidity}%</div>
+            <div>Ветер: {weather.wind.speed} м/с</div>
+            <div>Давление: {Math.round(weather.main.pressure * 0.75)} мм.рт.ст</div>
+          </div>
         </div>
-        // <div className="bg-blue-50 p-4 rounded">
-        //   <h4 className="font-bold text-xl">
-        //     {weather.name}, {weather.sys.country}
-        //   </h4>
-        //   <div className="flex items-center mt-2">
-        //     <img
-        //       src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-        //       alt={weather.weather[0].description}
-        //     />
-        //     <span className="text-2xl ml-2">
-        //       {Math.round(weather.main.temp)}°C
-        //     </span>
-        //   </div>
-        //   <p className="capitalize">{weather.weather[0].description}</p>
-        //   <div className="grid grid-cols-2 gap-2 mt-2 text-sm">
-        //     <div>Ощущается: {Math.round(weather.main.feels_like)}°C</div>
-        //     <div>Влажность: {weather.main.humidity}%</div>
-        //     <div>Ветер: {weather.wind.speed} м/с</div>
-        //     <div>Давление: {Math.round(weather.main.pressure * 0.75)} мм.рт.ст</div>
-        //   </div>
-        // </div>
       )}
     </>
   )
